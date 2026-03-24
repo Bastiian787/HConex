7f18bc78647e13ae23b30c26206b58604d1fe501
Bastiian787 <150836674+Bastiian787@users.noreply.github.com>
2026-03-24 19:45:21 -0300

    Create HConexApp.java

---
+++ src/main/java/com/hconex/HConexApp.java
@@ -0,0 +1,8 @@
+package com.hconex;
+
+public class HConexApp {
+    public static void main(String[] args) {
+        System.out.println("HConex Application Starting...");
+        // Initialize application here
+    }
+}
